ad9d0e623cff9b90355b71bd995e327046617f10
Abdulrazak Osman <zaak0101@hotmail.com>
2020-07-31 16:05:53 +0300

    v2.1 fixing bugs and issues!

--- extractor.py
+++ extractor.py
@@ -10,34 +10,37 @@ except Exception as e:
     print('Modules are Missing! \n\n {}'.format(e))
 
 # making the canvas
-root = Tk()
-root.geometry('700x400')
+app = Tk()
+app.geometry('600x200')
 # making the title
-root.title('Youtube Video Downloader')
+app.title('Youtube Video Downloader')
 
-# making labels 
-label_1 = Label(root, text='Enter The Link of The Video!', font=('bold',20))
-label_1.place(x=120, y=20)
+# making labels
+label_1 = Label(app,text="Enter The Link of The Video!", font=("bold",20))
+label_1.place(x=120,y=20)
 
-# declaring variables!
+label_2 = Label(app, text="Developed By Abdulrazak Osman", width=80, font=("bold",9))
+label_2.place(x=140,y=150)
+
+# declaring variables
 myLink = StringVar()
 
 # making the link entry bar!
-link_bar = Entry(root, width=60, textVariable=myLink)
+link_bar = Entry(app, width=60, textvariable=myLink)
 link_bar.place(x=140, y=80)
 
-# functions
+# main functions!
 def downloadVideo():
-    x_link = str(myLink.get())
-    video_url = YouTube(x_link).streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+    link = str(myLink.get())
+    video = YouTube(link).streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
 
     # checking if the folder is exists or make one!
     if not os.path.exists('./videos/'):
-        os.mkdir('./videos/')
-    video_url.download('./videos/')
+        os.makedirs('./videos/')
+    video.download('./videos/')
 
 # making the submiting button
-Button(root, text='Download', width=20, bg='green', fg='white', command=downloadVideo).place(x=220, y=110)
+Button(app,text="Download", width=20, bg='green',fg="white", command=downloadVideo).place(x=220, y=110)
 
 # this will run the app!
-root.mainloop()
+app.mainloop()
