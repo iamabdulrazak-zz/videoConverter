0e9bbafdced3613b1c76ec7a06be94ded19bf648
Abdulrazak Osman <zaak0101@hotmail.com>
2020-08-02 15:25:19 +0300

    v3.2 fixing bugs and imporvments

--- extractor.py
+++ extractor.py
@@ -1,32 +1,27 @@
-try:
-    # checking if the modules are installed
+try: # checking if the modules are installed
     from pytube import *    
     from tkinter import  *
     import os 
     
-    # printing a message if there is an issue with the packages!
-except Exception as e:
+except Exception as e: # printing a message if there is an issue with the packages!
     print('Modules are Missing! \n\n {}'.format(e))
 
-# making the canvas
-app = Tk()
-app.geometry('600x200')
-# making the title
-app.title('Youtube Video Downloader')
+app = Tk() # making the canvas
+app.geometry('600x200') # sizing the canvas
+app.title('Youtube Video Downloader') # making the title
 
-# making labels
-label_1 = Label(app,text="Enter The Link of The Video!", font=("bold",20))
-label_1.place(x=120,y=20)
+# making labels and it's place
+label_1 = Label(app,text="Enter YouTube Link!", font=("bold",20))
+label_1.place(x=180,y=20)
 
 label_2 = Label(app, text="Developed By Abdulrazak Osman", width=80, font=("bold",9))
-label_2.place(x=140,y=150)
+label_2.place(x=20,y=160)
 
-# declaring variables
-myLink = StringVar()
+myLink = StringVar() # link declaration!
 
-# making the link entry bar!
+# making the link entry bar and it's place!
 link_bar = Entry(app, width=60, textvariable=myLink)
-link_bar.place(x=140, y=80)
+link_bar.place(x=120, y=80)
 
 # main functions!
 def downloadVideo():
@@ -41,5 +36,4 @@ def downloadVideo():
 # making the submiting button
 Button(app,text="Download", width=20, bg='green',fg="white", command=downloadVideo).place(x=220, y=110)
 
-# this will run the app!
-app.mainloop()
+app.mainloop() # this will run the app!
